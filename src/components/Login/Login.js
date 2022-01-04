1511a44aa24f33b0cec5d05b3dbb4e63e6f4cd6f
inderjeetsinghbamrah <inderjeet.s.bamrah@gmail.com>
2022-01-04 22:48:11 +0530

    Auth Context Provider for Authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -24,6 +25,7 @@ const passwordReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 const Login = (props) => {
+  const authctx = useContext(AuthContext);
   //const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
   //const [enteredPassword, setEnteredPassword] = useState("");
@@ -32,12 +34,12 @@ const Login = (props) => {
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
-    isValid: false
+    isValid: false,
   });
 
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: "",
-    isValid: false
+    isValid: false,
   });
 
   const { isValid: emailisValid } = emailState;
@@ -74,7 +76,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
